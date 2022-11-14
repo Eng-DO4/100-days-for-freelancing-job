7030766fb96492c04da33bbd0514149451c7aa78
Eng-DO4 <mustafayasser1212@gmail.com>
2022-11-14 23:54:13 +0200

    bugs fixed

--- js/higherOrderFuncs/071-078/ass1.js
+++ js/higherOrderFuncs/071-078/ass1.js
@@ -2,7 +2,11 @@ let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
 
 // Elzero
 let conc = mix.map((elem) => {
-    Number.isNaN(parseInt(elem)) ? elem : '';
+    if(Number.isNaN(parseInt(elem))){
+        return elem
+    } else {
+        return ''
+    }
 }).reduce((acc, current) => acc += current);
 
 console.log(conc);
