77738ae7b864ad950dba59112634ae5d366a5fd3
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-17 20:03:54 +0200

    Now Pages

--- react/dojo-blog/src/App.js
+++ react/dojo-blog/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './NavBar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       {/* <NavBar> => will not work */}
       <NavBar /> {/* <NavBar></NavBar> => also works */}
+      <Home />
     </div>
   );
 }
