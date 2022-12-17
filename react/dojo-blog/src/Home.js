0e284674ae2b6c45007c3aee776d7e83e45ca893
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-17 21:01:05 +0200

    clean

--- react/dojo-blog/src/Home.js
+++ react/dojo-blog/src/Home.js
@@ -1,20 +1,7 @@
 const Home = () => {
-
-    function handleClick (e) {
-        console.log('Hello', e);
-    }
-
-    const handleClickAgain = (name) => {
-        console.log(`Hello, ${name}`);
-    }
-
     return (
         <div className="content">
             <h2>Home Page</h2>
-            <button onClick={ handleClick }>Click me</button>
-            <button onClick={ handleClickAgain }>Click me again</button> {/* but I want to pass args */}
-            {/* <button onClick={ handleClickAgain() }>Click me again</button> => will invoke the func automatically and return undefined*/}
-            <button onClick={ _ => handleClickAgain('dr4') }>The soln</button>
         </div>
     );
 }
