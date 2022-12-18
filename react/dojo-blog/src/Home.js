a6f771b86709117217114b93c516b5f6293c98ec
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-18 23:46:53 +0200

    useState hoo

--- react/dojo-blog/src/Home.js
+++ react/dojo-blog/src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from 'react'
+
 const Home = () => {
+
+    const [name, setName] = useState('dr4');
+    const [age, setAge] = useState(20);
+
+    const handleclick = _ => {
+        setName('DO4');
+        setAge(21);
+    }
+
     return (
         <div className="content">
             <h2>Home Page</h2>
+            <br />
+            <button onClick={handleclick}>Click me</button>
+            <br /><br />
+            <p>Hello my name is {name}, {age} years old.</p>
         </div>
     );
 }
