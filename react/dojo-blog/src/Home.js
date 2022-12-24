2694a34002f9f37037a26a440d21013a0e1bc153
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-24 17:09:53 +0200

    using fetch with useEffect hook

--- react/dojo-blog/src/Home.js
+++ react/dojo-blog/src/Home.js
@@ -6,12 +6,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState(null);
 
     useEffect( () => {
-        
-    }, []);
+        fetch('http://localhost:8000/blogs').then(res => res.json()).then(data => setBlogs(data));
+    }, []); // I want it one time only after loading the page
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs!'/>
+            {/* <BlogList blogs={blogs} title='All Blogs!'/> => will not work and gets an error */}
+            { blogs && <BlogList blogs={blogs} title='All Blogs!'/> } {/* && => checks left side first */}
         </div>
     );
 }
