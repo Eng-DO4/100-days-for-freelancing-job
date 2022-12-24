c478adbdfa2e32c43c00ff6a38463b42d34d1b95
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-24 17:37:55 +0200

    simulating fetching from remote server

--- react/dojo-blog/src/Home.js
+++ react/dojo-blog/src/Home.js
@@ -4,13 +4,20 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
-    useEffect( () => {
-        fetch('http://localhost:8000/blogs').then(res => res.json()).then(data => setBlogs(data));
-    }, []); // I want it one time only after loading the page
+    useEffect( _ => {
+        setTimeout( _ => {
+            fetch('http://localhost:8000/blogs')
+             .then(res => res.json())
+             .then(data => {
+                setBlogs(data);
+                setIsPending(false)
+        })}, 1000)}, []); // I want it one time only after loading the page
 
     return (
         <div className="home">
+            { isPending && <div>Loading...</div>}
             {/* <BlogList blogs={blogs} title='All Blogs!'/> => will not work and gets an error */}
             { blogs && <BlogList blogs={blogs} title='All Blogs!'/> } {/* && => checks left side first */}
         </div>
