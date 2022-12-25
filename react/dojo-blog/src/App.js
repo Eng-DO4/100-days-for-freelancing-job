d4af7bc46262e4940f4ae2f78f9a1569eaa53cd0
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-25 23:45:30 +0200

    installing & using react-router-dom

--- react/dojo-blog/src/App.js
+++ react/dojo-blog/src/App.js
@@ -1,12 +1,19 @@
 import NavBar from './NavBar';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route path='/'> {/* depends on the route provided */}
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
