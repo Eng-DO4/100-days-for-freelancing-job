89bd6064dc01c2ed1ad2be86d007527ef34036bc
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-28 16:07:36 +0200

    making a delete request

--- react/dojo-blog/src/BlogDetails.js
+++ react/dojo-blog/src/BlogDetails.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams(); // to get the params from query statment
-
     const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
+    const history = useHistory();
+
+    const handleClick = _ => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: "DELETE"
+        }).then( _ => history.push('/'));
+    }
 
     return (
         <div className="blog-details">
@@ -17,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={ handleClick }>Delete</button>
                 </article>
             )}
         </div>
