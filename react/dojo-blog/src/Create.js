dee93310b22f7307a66340c3ca1f49e100067445
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-28 15:40:44 +0200

    making a post request

--- react/dojo-blog/src/Create.js
+++ react/dojo-blog/src/Create.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+    // storing inputed values in variables
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('DR4');
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setIsPending(true);
+        const blog = { title, body, author };
+        fetch('http://localhost:8000/blogs', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then( _ => { setIsPending(false); });
+    }
+
     return (
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <div className="title-field">
                     <label htmlFor="blog-title">Blog Title</label>
-                    <input type="text" name="title" id="blog-title" />
+                    <input type="text" name="title" id="blog-title" required value={title} 
+                     onChange={ e => setTitle(e.target.value) } /> {/* this func updates the value */}
                 </div>
                 <div className="body-field">
                     <label htmlFor="blog-body">Blog Body</label>
-                    <textarea name="body" id="blog-body" cols="55" rows="10"></textarea>
+                    <textarea name="body" id="blog-body" cols="55" rows="10" required value={body} 
+                     onChange={ e => setBody(e.target.value) }></textarea>
                 </div>
                 <div className="author-field">
                     <label htmlFor="blog-author">Blog Author</label>
-                    <select name="author" id="blog-author">
+                    <select name="author" id="blog-author" value={author} required 
+                     onChange={ e => setAuthor(e.target.value) }>
                         <option value="DR4">DR4</option>
                         <option value="DO4">DO4</option>
                     </select>
                 </div>
-                <button>Add</button>
+                { !isPending && <button>Add</button> }
+                { isPending && <button disabled>Add</button> } {/* disables the button till the blog added */}
             </form>
         </div>
     );
