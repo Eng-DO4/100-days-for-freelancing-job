4cbe55d5eb264c2123b383959f27e14f4622d77e
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-28 14:27:28 +0200

    design a form and style it

--- react/dojo-blog/src/Create.js
+++ react/dojo-blog/src/Create.js
@@ -3,7 +3,25 @@ import React from 'react'
 const Create = () => {
     return (
         <div className="create">
-            <h2>New Blogs!</h2>
+            <h2>Add a new Blog</h2>
+            <form>
+                <div className="title-field">
+                    <label htmlFor="blog-title">Blog Title</label>
+                    <input type="text" name="title" id="blog-title" />
+                </div>
+                <div className="body-field">
+                    <label htmlFor="blog-body">Blog Body</label>
+                    <textarea name="body" id="blog-body" cols="55" rows="10"></textarea>
+                </div>
+                <div className="author-field">
+                    <label htmlFor="blog-author">Blog Author</label>
+                    <select name="author" id="blog-author">
+                        <option value="DR4">DR4</option>
+                        <option value="DO4">DO4</option>
+                    </select>
+                </div>
+                <button>Add</button>
+            </form>
         </div>
     );
 }
