e35d063a073a47be7d6205a326de703b60ab3fd3
Eng-DO4 <mustafayasser1212@gmail.com>
2023-01-16 20:22:30 +0200

    toggle and active class done

--- templates/2/js/main.js
+++ templates/2/js/main.js
@@ -1,7 +1,24 @@
+// calling elements in DOM
 const toggle_icon = document.querySelector('.fa-bars');
 const menu = document.querySelector('header ul');
+const navLinks = document.querySelectorAll('header ul a')
 
-toggle_icon.addEventListener('click', e => {
-    e.preventDefault();
-    // to be cont...
+// media query condition
+let media = window.matchMedia("(max-width: 799px)")
+menu.style.display = media.matches ? 'none' : 'flex'
+media.onchange = _ => {
+    menu.style.display = media.matches ? 'none' : 'flex'
+}
+
+// toggling the menu
+let toggle = _ => menu.style.display = (menu.style.display == 'none') ? 'flex' : 'none';
+toggle_icon.addEventListener('click', toggle);
+
+// hovering on nav links
+navLinks.forEach(a => {
+    a.onclick = _ => {
+        navLinks.forEach(aClass => aClass.classList.remove('active'))
+        a.classList.add('active')
+        menu.style.display = media.matches ? 'none' : 'flex'
+    }
 })
