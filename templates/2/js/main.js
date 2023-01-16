1ad4ced901616afd077c808adb8852cb14dbd196
Eng-DO4 <mustafayasser1212@gmail.com>
2023-01-16 21:02:12 +0200

    header is done with perfect touches

--- templates/2/js/main.js
+++ templates/2/js/main.js
@@ -1,13 +1,14 @@
 // calling elements in DOM
 const toggle_icon = document.querySelector('.fa-bars');
 const menu = document.querySelector('header ul');
-const navLinks = document.querySelectorAll('header ul a')
+const navLinks = document.querySelectorAll('header ul a');
+const logo = document.querySelector('header .logo')
 
 // media query condition
 let media = window.matchMedia("(max-width: 799px)")
-menu.style.display = media.matches ? 'none' : 'flex'
+menu.style.display = media.matches ? 'none' : 'flex';
 media.onchange = _ => {
-    menu.style.display = media.matches ? 'none' : 'flex'
+    menu.style.display = media.matches ? 'none' : 'flex';
 }
 
 // toggling the menu
@@ -15,10 +16,15 @@ let toggle = _ => menu.style.display = (menu.style.display == 'none') ? 'flex' :
 toggle_icon.addEventListener('click', toggle);
 
 // hovering on nav links
+logo.onclick = _ => {
+    navLinks.forEach(aClass => aClass.classList.remove('active'));
+    navLinks[0].classList.add('active');
+    menu.style.display = media.matches ? 'none' : 'flex';
+}
 navLinks.forEach(a => {
     a.onclick = _ => {
-        navLinks.forEach(aClass => aClass.classList.remove('active'))
-        a.classList.add('active')
-        menu.style.display = media.matches ? 'none' : 'flex'
+        navLinks.forEach(aClass => aClass.classList.remove('active'));
+        a.classList.add('active');
+        menu.style.display = media.matches ? 'none' : 'flex';
     }
 })
