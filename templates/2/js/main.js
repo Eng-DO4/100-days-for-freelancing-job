4a428a5755a6e93e8e5ad1d03efc591abcf27d17
Eng-DO4 <mustafayasser1212@gmail.com>
2023-01-16 13:05:08 +0200

    toggle effect

---
+++ templates/2/js/main.js
@@ -0,0 +1,7 @@
+const toggle_icon = document.querySelector('.fa-bars');
+const menu = document.querySelector('header ul');
+
+toggle_icon.addEventListener('click', e => {
+    e.preventDefault();
+    // to be cont...
+})
