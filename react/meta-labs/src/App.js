4c78629ccd5b248f05b915a4859685709bc94916
Eng-DO4 <mustafayasser1212@gmail.com>
2023-04-16 17:10:45 +0200

    lab4

--- react/meta-labs/src/App.js
+++ react/meta-labs/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Heading from './Heading'
+import Card from './Card'
 
 function App() {
   return (
-    <>
-      <Heading firstName="Bob" />
-      <Heading firstName="DR4" />
-    </>
+    <div className="App">
+      <Card h2="First Card's h2" h3="First Card's h3" />
+      <Card h2="second Card's h2" h3="second Card's h3" />
+      <Card h2="third Card's h2" h3="third Card's h3" />
+    </div>
   );
 }
 
